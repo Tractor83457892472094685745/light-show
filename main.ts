625e4b318c4c0eefcd46788e6dd36fdbff20aa5a
Tractor <101215336+Tractor83457892472094685745@users.noreply.github.com>
2022-03-23 13:56:34 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,34 +1,34 @@
 basic.forever(function () {
     if (randint(1, 8) == 1) {
         mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Red)
-        basic.pause(100)
+        basic.pause(200)
     }
     if (randint(1, 8) == 2) {
         mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.OFF)
-        basic.pause(100)
+        basic.pause(200)
     }
     if (randint(1, 8) == 3) {
         mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Green)
-        basic.pause(100)
+        basic.pause(200)
     }
     if (randint(1, 8) == 4) {
         mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Blue)
-        basic.pause(100)
+        basic.pause(200)
     }
     if (randint(1, 8) == 5) {
         mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.White)
-        basic.pause(100)
+        basic.pause(200)
     }
     if (randint(1, 8) == 6) {
         mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Cyan)
-        basic.pause(100)
+        basic.pause(200)
     }
     if (randint(1, 8) == 7) {
         mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Pinkish)
-        basic.pause(100)
+        basic.pause(200)
     }
     if (randint(1, 8) == 8) {
         mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Yellow)
-        basic.pause(100)
+        basic.pause(200)
     }
 })
