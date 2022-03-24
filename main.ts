a54ce00e697deed12b2a5597a4ef25192492094c
Tractor <101215336+Tractor83457892472094685745@users.noreply.github.com>
2022-03-24 09:27:44 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,32 +2,25 @@ basic.forever(function () {
     if (randint(1, 8) == 1) {
         mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Red)
         basic.pause(200)
-    }
-    if (randint(1, 8) == 2) {
-        mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.OFF)
-        basic.pause(200)
-    }
-    if (randint(1, 8) == 3) {
-        mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Green)
-        basic.pause(200)
-    }
-    if (randint(1, 8) == 4) {
+    } else if (randint(1, 8) == 2) {
         mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Blue)
         basic.pause(200)
-    }
-    if (randint(1, 8) == 5) {
+    } else if (randint(1, 8) == 3) {
+        mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.OFF)
+        basic.pause(200)
+    } else if (randint(1, 8) == 4) {
+        mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Green)
+        basic.pause(200)
+    } else if (randint(1, 8) == 5) {
         mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.White)
         basic.pause(200)
-    }
-    if (randint(1, 8) == 6) {
+    } else if (randint(1, 8) == 6) {
         mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Cyan)
         basic.pause(200)
-    }
-    if (randint(1, 8) == 7) {
+    } else if (randint(1, 8) == 7) {
         mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Pinkish)
         basic.pause(200)
-    }
-    if (randint(1, 8) == 8) {
+    } else if (randint(1, 8) == 8) {
         mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Yellow)
         basic.pause(200)
     }
